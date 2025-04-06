577a95072bdd72e203c3844a2facf433b8142f9d
tom <thomascoote89@gmail.com>
2025-04-06 10:42:22 +0100

    intitial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from turtle import Turtle, Screen
+
+#TODO 1 - 800x800 screen
+
+#TODO 2 - Turtle at bottom of screen. can only move forward with a key press
+
+#TODO 3 - Cars of different colours (and speeds?) pass the screen from right to left.
+
+#TODO 4 - Collision detection for car hitting turtle. If hit, game over.
+
+#TODO 5 - Win sreen for reaching the other side of the road
+
+#TODO 6 - Scoreboard, and increase speed of cars with each successful game.
+
+screen = Screen()
+screen_width = 800
+screen_height = 800
+
+screen.setup(width=800,height=800,)
+
